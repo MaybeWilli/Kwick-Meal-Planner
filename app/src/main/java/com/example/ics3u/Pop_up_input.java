a79305b3019a4e193f051acb2551d9567537cb40
kdu4 <kdu4@ocdsb.ca>
2022-05-02 17:42:11 -0400

    publishing

---
+++ app/src/main/java/com/example/ics3u/Pop_up_input.java
@@ -0,0 +1,14 @@
+package com.example.ics3u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pop_up_input extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_up_input);
+    }
+}
