7cfa60d6a316e5dedf3c057aca442b8804355b25
MaybeWilli <51888361+MaybeWilli@users.noreply.github.com>
2022-06-06 10:51:41 -0400

    Part 2

--- app/src/main/java/com/example/ics3u/EditMealView.java
+++ app/src/main/java/com/example/ics3u/EditMealView.java
@@ -1,10 +1,13 @@
 package com.example.ics3u;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditMealView extends AppCompatActivity {
+    public TextView nameTV;
+    public int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
